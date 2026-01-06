fd0df969f24b6196f69479ff9a8881a73cc7306d
habiburrahman30 <hmhabib999@gmail.com>
2026-01-06 18:31:52 +0600

    fix:build issue

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  return NextResponse.json({ success: true });
+export async function GET() {
+  return NextResponse.json({ ok: true });
 }
